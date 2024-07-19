792cccd2ab2347b2b626c325b9354cfe7e21d31b
artArthur021 <675050500155@mail.rmutk.ac.th>
2024-07-19 16:12:05 +0700

    ส่งงาน

--- EP1/index.ts
+++ EP1/index.ts
@@ -6,3 +6,10 @@ let อยากกลับบ้าน: boolean = false
 console.log(firstName)
 console.log(y)
 console.log(อยากกลับบ้าน)
+
+const a: number = 1
+const b: number = 2
+
+console.log (a == b)
+console.log (a > b)
+console.log (a<= b)
